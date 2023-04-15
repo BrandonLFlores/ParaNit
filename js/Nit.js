c23985142b32f4eaf9c2ea1c02f0e0adbaa3db2f
BrandonLFlores <brandonleof@gmail.com>
2023-04-15 14:19:47 -0300

    paraNit

---
+++ js/Nit.js
@@ -0,0 +1,14 @@
+respuesta=prompt("¿Quieres a Brandon?[si/no]");
+
+while(respuesta!='si' && respuesta!='Si' && respuesta!='SI' && respuesta!='sI' && respuesta!='no' && respuesta!='No' && respuesta!='NO' && respuesta!='nO'){
+    respuesta=prompt("¿Quieres a Brandon?[si/no]");
+}
+
+if(respuesta=='si' || respuesta=='Si' || respuesta=='SI' || respuesta=='sI')
+{
+    alert("Brandon también te quiere mucho, y lo seguirá haciendo hasta el fin de los tiempos, porque vos sos su Reina 💘👑");
+}
+else if(respuesta=='no' || respuesta=='No' || respuesta=='NO' || respuesta=='nO')
+{
+    alert("Pus bueno, no pasa nada, porque Brandon te quiere mucho, y no importa lo que pase, él siempre estará allí para ti 💘👑");
+}
